1c93562eea54918aab21bb83f401dc33513a56ae
dantinhoz <dante.cf.lopes@gmail.com>
2025-08-12 12:11:41 -0300

    pequenas modificações

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-nomeInput = document.getElementsByClassName("caixaNome");
-emailInput = document.getElementsByClassName("caixaEmail");
+let nomeInput = document.getElementsByClassName("caixaNome");
+let emailInput = document.getElementsByClassName("caixaEmail");
 
 function alerta() {
-  if (nomeInput.length == 0 || emailInput.length == 0) {
-    alert("Preencha os dados obrigatórios para enviar!");
-  } else {
+  if (nomeInput.length != 0 && emailInput.length != 0) {
     alert("Mensagem e contato enviados!");
+  } else {
+    alert("Preencha os dados obrigatórios para enviar!");
   }
+  console.log(nomeInput, emailInput)
 }
