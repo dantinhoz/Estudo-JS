7e64e01fdda3ff81a3df210be287249c584723ab
dantinhoz <dante.cf.lopes@gmail.com>
2025-08-18 12:15:22 -0300

    início das configurações do menu

--- script.js
+++ script.js
@@ -13,4 +13,11 @@ function alerta() {
 }
 
 // FUNÇÃO DO MENU
-function abrirMenu() {}
+function abrirMenu() {
+  document.getElementsByClassName("overlayMenu")[0].style.width = "25%";
+  document.getElementsByClassName("svgMenu")[0].path = "M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708";
+}
+
+function fecharMenu() {
+  document.getElementsByClassName("overlayMenu")[0].style.width = "0";
+}
